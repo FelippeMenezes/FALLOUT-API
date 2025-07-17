3365ff96635619054ab2651bb0a102585761a343
Felippe Santana <felippemenezes1982@gmail.com>
2025-07-17 17:21:23 -0300

    fix: Remove asset precompilation tasks from build script for API-only app

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,10 +5,6 @@ set -o errexit
 # Instala as dependências
 bundle install
 
-# Compila os assets (necessário para projetos Rails não-API, mas não custa ter)
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-
 # Executa as migrações e o seed do banco de dados
 bundle exec rake db:migrate
 bundle exec rake db:seed
