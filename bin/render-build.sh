cc0d6d3d5df09cf0bd83839fe02c271efbe2b73a
Felippe Santana <felippemenezes1982@gmail.com>
2025-07-17 16:33:27 -0300

    feat: Configure build script for Render deployment

---
+++ bin/render-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Instala as dependências
+bundle install
+
+# Compila os assets (necessário para projetos Rails não-API, mas não custa ter)
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+
+# Executa as migrações e o seed do banco de dados
+bundle exec rake db:migrate
+bundle exec rake db:seed
