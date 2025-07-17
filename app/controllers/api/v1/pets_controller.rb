0a75215a77614a7373ad9b60c464e5fe29d18bd9
Felippe Santana <felippemenezes1982@gmail.com>
2025-07-17 16:01:35 -0300

    Rarity order

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::PetsController < ApplicationController
   def index
-    pets = Pet.all.order(:name)
+    order_clause = "CASE rarity WHEN 'Lendário' THEN 1 WHEN 'Raro' THEN 2 WHEN 'Comum' THEN 3 ELSE 4 END, name ASC"
+    pets = Pet.order(Arel.sql(order_clause))
     render json: pets
   end
 end
